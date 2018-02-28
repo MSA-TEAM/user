9eefb71cd9cde7f85ddcbbe7796a391a223267ed
leonardo.park <blue.park@kt.com>
2018-02-28 18:00:18 +0900

    change configuration

--- src/main/java/com/ktds/msa/user/controller/UserController.java
+++ src/main/java/com/ktds/msa/user/controller/UserController.java
@@ -1,28 +1,17 @@
 package com.ktds.msa.user.controller;
 
-import java.net.URI;
-
-import javax.annotation.Resource;
-
-import org.apache.ibatis.session.SqlSession;
+import com.ktds.msa.user.common.exception.UserNotFoundException;
+import com.ktds.msa.user.domain.User;
+import com.ktds.msa.user.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-
-import com.ktds.msa.user.common.exception.UserNotFoundException;
-import com.ktds.msa.user.domain.User;
-import com.ktds.msa.user.service.UserService;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/user")
